2a04eb33202d81aa0f9141a2fdeab0d7f6dcea03
kakarot <kakarotzcloud@gmail.com>
2026-05-10 00:55:54 +0530

    added terraform block

---
+++ terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "kakarot-terraform-backend"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
